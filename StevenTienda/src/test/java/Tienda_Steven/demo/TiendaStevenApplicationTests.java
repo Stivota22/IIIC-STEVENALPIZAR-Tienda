3f193c5a035307229f2d61828fc3655450b68b34
Steven A <60278482+Stivota22@users.noreply.github.com>
2023-10-11 19:24:26 -0600

    materia vista antes del recreo

---
+++ StevenTienda/src/test/java/Tienda_Steven/demo/TiendaStevenApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Steven.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaStevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
